e5141a51b9b6f078cb35ff854636a6c99c3eeb14
mojib2014 <mojib_monir@yahoo.com>
2021-05-25 16:03:55 -0700

    initial commit

---
+++ parser.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const nightmare = require("nightmare")();
+
+const args = process.argv.slice(2);
+const url = args[0];
+const minPrice = args[1];
+
+checkPrice();
+
+async function checkPrice() {
+  try {
+    const priceString = await nightmare
+      .goto(url)
+      .wait("#priceblock_ourprice")
+      .evaluate(() => document.getElementById("priceblock_ourprice").innerText)
+      .end();
+    const priceNumber = parseFloat(priceString.replace("$", ""));
+    if (priceNumber < minPrice) {
+      try {
+        asdffa;
+        await sendEmail(
+          "Price Is Low",
+          `The price on ${url} has dropped below ${minPrice}`,
+        );
+      } catch (error) {
+        console.log("My Error", error.response.body);
+      }
+    }
+  } catch (e) {
+    try {
+      await sendEmail("Amazon Price Checker Error", e.message);
+    } catch (error) {
+      console.log(error);
+    }
+    throw e;
+  }
+}
+
+function sendEmail(subject, body) {
+  const email = {
+    to: "matogiy672@itwbuy.com",
+    from: "mmohammad3@outlook.com",
+    subject: subject,
+    text: body,
+    html: body,
+  };
+
+  return sgMail.send(email);
+}
